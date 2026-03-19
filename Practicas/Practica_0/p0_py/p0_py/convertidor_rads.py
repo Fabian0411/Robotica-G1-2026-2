503123b3d55365370cae3033f2e44dd6debe65ed
Fabian <eltiofabs@gmail.com>
2026-03-18 23:18:46 -0600

    Finaliza Práctica 0: Nodos ROS 2, gráfica rqt_plot y reporte completo

--- Practicas/Practica_0/p0_py/p0_py/convertidor_rads.py
+++ Practicas/Practica_0/p0_py/p0_py/convertidor_rads.py
@@ -1,33 +1,29 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
+from std_msgs.msg import Float64
 import math
 
 class ConvertidorRads(Node):
     def __init__(self):
         super().__init__('convertidor_rads')
         
-        # 1. Se suscribe al tópico 'rpm' (escucha)
         self.subscription = self.create_subscription(
-            Float32,
+            Float64,
             'rpm',
             self.listener_callback,
             10)
             
-        # 2. Crea un publicador en el tópico 'rad_s' (habla)
-        self.publisher_ = self.create_publisher(Float32, 'rad_s', 10)
+        self.publisher_ = self.create_publisher(Float64, 'rad_s', 10)
 
     def listener_callback(self, msg):
         rpm = msg.data
-        # Aplicamos la transformación matemática
         rad_s = rpm * (2.0 * math.pi / 60.0)
         
-        # Creamos el nuevo mensaje y lo publicamos
-        nuevo_msg = Float32()
+        nuevo_msg = Float64()
         nuevo_msg.data = rad_s
         self.publisher_.publish(nuevo_msg)
         
-        self.get_logger().info(f'Recibido: {rpm:.2f} RPM | Transformado: {rad_s:.2f} rad/s')
+        self.get_logger().info(f'Recibido: {rpm:.4f} RPM | Transformado: {rad_s:.4f} rad/s')
 
 def main(args=None):
     rclpy.init(args=args)
--- Practicas/Practica_0/p0_py/p0_py/generador_rpm.py
+++ Practicas/Practica_0/p0_py/p0_py/generador_rpm.py
@@ -1,29 +1,23 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32
+from std_msgs.msg import Float64
 import math
 import time
 
 class GeneradorRPM(Node):
     def __init__(self):
-        # Nombramos el nodo
         super().__init__('generador_rpm')
-        # Creamos el publicador en el tópico 'rpm'
-        self.publisher_ = self.create_publisher(Float32, 'rpm', 10)
-        # Hacemos que publique cada 0.1 segundos (10 Hz)
+        self.publisher_ = self.create_publisher(Float64, 'rpm', 10)
         timer_period = 0.1 
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.start_time = time.time()
 
     def timer_callback(self):
-        msg = Float32()
-        # Calculamos el tiempo transcurrido
+        msg = Float64()
         t = time.time() - self.start_time
-        # Generamos la señal senoidal (Amplitud de 100)
-        msg.data = 100.0 * math.sin(t)
-        
+        msg.data = 0.5 * math.sin(t) + 0.5
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publicando: {msg.data:.2f} RPM')
+        self.get_logger().info(f'Publicando: {msg.data:.4f} RPM')
 
 def main(args=None):
     rclpy.init(args=args)
